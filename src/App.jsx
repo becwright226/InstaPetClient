a402ee163cba1fe85696bec94661c8a6a64dd871
becwright226 <becwright226@gmail.com>
2022-02-10 14:53:25 -0500

    Folders stubbed, app 'Hello' tested

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import React, { useState, useEffect } from 'react';
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Hello from Instapet App.jsx</h2>
+    </div>
+  );
+}
+
+export default App;
